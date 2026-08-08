c74ba28a463483ac9dd0bd3aaca1069ca773a26f
can6a1 <cannabinos6a1@gmail.com>
2026-08-08 17:12:32 -0300

    Buscar en Google "Hola"

---
+++ AbrirGoogleMSTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AbrirGoogleMSTest
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void BuscarGoogle()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://google.com");
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("Hola");
+            element.Submit();
+
+        }
+    }
+}
